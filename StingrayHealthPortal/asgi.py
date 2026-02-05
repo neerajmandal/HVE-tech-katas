6216682650064e5d8d369a4b5e21fd85183ebb69
Neeraj Mandal <mandalneeraj21@gmail.com>
2026-02-04 16:25:26 -0800

    changed to pataient portal

--- StingrayHealthPortal/asgi.py
+++ StingrayHealthPortal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for StingraysCarePayments project.
+ASGI config for StingrayHealthPortal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StingraysCarePayments.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StingrayHealthPortal.settings")
 
 application = get_asgi_application()
